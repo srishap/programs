9e1b0d656de56afbf8345ca76c61192a6e28d0ec
temporary <temporary@Pyati>
2021-03-22 20:07:27 +0530

    up

--- Practise/src/fact.java
+++ Practise/src/fact.java
@@ -6,16 +6,16 @@ public class fact
 		int fact=1;
 		for(int i=1;i<=n;i++)
 		{
-			fact=fact*i;
+			fact=fact*i;//formula
 		}
-		return fact;
+		return fact;//return fact
 	}	
 
 	public static void main(String[] args)
 	{
 		
 	int n=6;
-		System.out.println("fact of " + n + "is " +fact(n));
+		System.out.println("fact of " + n + "is " +fact(n));//in the form of 4 is ans
 		
 
 	}
